0cae0f29bda21b9aeb36d84561833f66023ec066
aalexsoloveiko <aalexsoloveiko@gmail.com>
2019-03-15 15:42:09 +0300

    feat: implement solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+  let newZeros = Number.MAX_VALUE;
+  let newBase = base;
+  for (let i = 2; i <= base; i++) {
+    if (newBase % i === 0) {
+      let divider = 0;
+      while (newBase % i === 0) {
+        newBase = newBase / i;
+        divider++;
+      }
+      let dividend = 0;
+      let z = Math.floor(number / i);
+      while (z > 0) {
+        dividend += z;
+        z = Math.floor(z / i);
+      }
+     newZeros = Math.min (newZeros, Math.floor(dividend / divider))
+    }
+  }
+  return newZeros;
+};
